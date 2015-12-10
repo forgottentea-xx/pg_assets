081bc2183d85b010a43c21703c4368ff4cb50a90
Andy Hartford <andy.hartford@cohealo.com>
2015-12-10 10:12:29 -0500

    Changes to load paths and dependency versions

--- lib/pg_assets/railtie.rb
+++ lib/pg_assets/railtie.rb
@@ -12,18 +12,7 @@ module PgAssets
     }
 
     rake_tasks do
-      load '../../tasks/pg_assets.rake'
-    end
-
-    # some of the column types in the pg_catalog views are not registered.
-    # We don't really need them, but we'll register them so we don't get ugly
-    # error messages
-    initializer 'pg_assets.register_types' do
-      UNREGISTERED_TYPES.each_pair do |pgtype, aliastype|
-        unless ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::OID.registered_type?(pgtype)
-          ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::OID.alias_type(pgtype.to_s, aliastype)
-        end
-      end
+      load 'tasks/pg_assets.rake'
     end
   end
 end
