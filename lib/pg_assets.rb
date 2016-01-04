12e92f2656a53c16cbb4149b91b06a4244e4e458
Jeff Osborn <jeffrey.osborn@autodesk.com>
2016-01-04 12:30:23 -0500

    Load multiple versions of rails on travis. Changed the way those oid warnings are silenced, to track activerecord versions There is still a singular warning on rails 4.2

--- lib/pg_assets.rb
+++ lib/pg_assets.rb
@@ -1,14 +1,14 @@
 require 'pg'
 
-require File.expand_path('../pg_assets/models/concerns/loadable_asset', __FILE__)
-require File.expand_path('../pg_assets/models/pg_constraint', __FILE__)
-require File.expand_path('../pg_assets/models/pg_trigger', __FILE__)
-require File.expand_path('../pg_assets/models/pg_mat_view', __FILE__)
-require File.expand_path('../pg_assets/models/pg_view', __FILE__)
-require File.expand_path('../pg_assets/models/pg_function', __FILE__)
-require File.expand_path('../pg_assets/services/pg_asset_manager', __FILE__)
-require File.expand_path('../pg_assets/helpers/views_migration_helper', __FILE__)
-require File.expand_path('../pg_assets/version', __FILE__)
+require_relative 'pg_assets/models/concerns/loadable_asset'
+require_relative 'pg_assets/models/pg_constraint'
+require_relative 'pg_assets/models/pg_trigger'
+require_relative 'pg_assets/models/pg_mat_view'
+require_relative 'pg_assets/models/pg_view'
+require_relative 'pg_assets/models/pg_function'
+require_relative 'pg_assets/services/pg_asset_manager'
+require_relative 'pg_assets/helpers/views_migration_helper'
+require_relative 'pg_assets/version'
 
 module PgAssets
   require 'pg_assets/railtie' if defined?(Rails)
--- pg_assets.gemspec
+++ pg_assets.gemspec
@@ -15,7 +15,7 @@ Gem::Specification.new do |s|
   s.files = Dir["{lib}/**/*", "MIT-LICENSE", "Rakefile", "README.rdoc"]
   s.test_files = Dir["test/**/*"]
 
-  s.add_dependency "rails", "~> 4.0.12"
+  s.add_dependency "rails", "~> 4"
 
   s.add_dependency "pg"
   s.add_development_dependency "minitest"
